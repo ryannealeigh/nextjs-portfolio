8588ea89b3ac55328929b5104baef57e3d3b144a
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2024-10-25 10:10:19 -0700

    chore: edit frontpage copy, rss feed copy

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,15 @@ export default function Page() {
         Ryan Nealeigh
       </h1>
       <p className="mb-4">
-        {`I'm a coffee enthusiast and fountain pen aficionado.`}
+        I'm a coffee enthusiast and fountain pen aficionado, currently working at <a href="https://amazon.com">Amazon</a>,
+        where I design and build flexible APIs and backend services that improve data accessibility, streamline complex
+        processes, and empower developers across diverse projects. Outside of work, I’m often experimenting with pour-over
+        coffee, meticulously weighing beans and fine-tuning each cup. I also enjoy writing and organizing thoughts in my
+        Leuchtturm1917 notebook with a Hongdian fountain pen. A huge science fiction and fantasy fan, I love getting lost in worlds like 
+        <em>The Lord of the Rings</em>, 
+        <em>The Cosmere</em>, 
+        <em>The Expanse</em>, and the 
+        <em>Halo</em> novel series.
       </p>
       <div className="my-8">
         <BlogPosts />
