a74b2b496229af27ed09b0bbb2217a9d35c178f4
Ryan Nealeigh <ryan.nealeigh@gmail.com>
2024-10-24 21:37:33 -0700

    chore: get site building and update copy

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ import { baseUrl } from './sitemap'
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: 'Next.js Portfolio Starter',
-    template: '%s | Next.js Portfolio Starter',
+    default: 'Ryan Nealeigh | Software Engineer',
+    template: '%s | Ryan Nealeigh | Software Engineer',
   },
   description: 'This is my portfolio.',
   openGraph: {
